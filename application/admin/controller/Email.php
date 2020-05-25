824488f932bb2855dc0dcdd9bcbfaac9eaced0ff
value94 <x_x9403@163.com>
2020-05-25 10:57:46 +0800

    add email import

--- application/admin/controller/Email.php
+++ application/admin/controller/Email.php
@@ -102,6 +102,70 @@ class Email extends Base
     }
 
     /**
+     * 批量导入邮箱
+     * @return mixed
+     */
+    public function import_email()
+    {
+        if (request()->isPost()) {
+            // 获取表单上传文件
+            $file = request()->file('email_file');
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->move('../uploads');
+            if ($info) {
+                // 获取数据,循环处理
+                $path = $info->getPathname();
+                // 读取数据
+                $orders_data = $this->data_import($path);
+                if (!$orders_data) {
+                    $this->error('excel解析失败,请检查格式');
+                }
+            } else {
+                // 上传失败获取错误信息
+                $this->error('上传文件失败,请重试!');
+            }
+
+            // 添加数据
+            foreach ($orders_data as $c) {
+                // 判断邮箱是否已存在
+                $Email = new EmailModel();
+                $res = $Email->where('email_name', '=', $c[0])->find();
+                if ($res) {
+                    continue;
+                } else {
+                    try {
+                        $Email->email_name = $c[0];
+                        $Email->email_password = $c[1];
+                        $Email->pop3svr = $c[2];
+                        $Email->smtpsvr = $c[3];
+                        $Email->save();
+                    } catch (\Exception $e) {
+                        $this->error('添加失败,请重试');
+                    }
+                }
+            }
+            $this->success('批量添加成功');
+        }
+        return $this->fetch();
+    }
+
+    //创建一个读取excel数据，可用于入库
+    function data_import($filename)
+    {
+        try {
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($filename);
+            $sheet = $spreadsheet->getActiveSheet();
+            $excel_array = $sheet->toArray();
+
+            array_shift($excel_array);  //删除第一个数组(标题);
+
+            return $excel_array;
+        } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            $this->error('excel解析错误,请重试!');
+        }
+    }
+
+    /**
      * 拼装操作按钮
      * @param $id
      * @return array
