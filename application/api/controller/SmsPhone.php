1829c25e121c2263b7762fa0fd0d904b8ae817bc
value94 <x_x9403@163.com>
2020-07-28 21:17:21 +0800

    add getTashToken set phone relise

--- application/api/controller/SmsPhone.php
+++ application/api/controller/SmsPhone.php
@@ -71,11 +71,15 @@ class SmsPhone extends Controller
         // 验证参数
         $params = (new GetTaskTokenValidate())->goCheck();
 
+        // 设置手机解除异常
         // 验证手机号
         $check_phone = Cache::get('sms_' . $params['phone_sn']);
 
         // 更新最后一次验证时间
-        SmsPhoneModel::update(['last_get_time' => date('Y-m-d H:i:s')], ['phone_sn' => $params['phone_sn']]);
+        SmsPhoneModel::update([
+            'last_get_time' => date('Y-m-d H:i:s'),
+            'status' => 0,
+        ], ['phone_sn' => $params['phone_sn']]);
 
         if (!$check_phone) {
             throw new SmsPhoneException([
