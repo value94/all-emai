008809749e135afec2035e6f403c745ae60f60b6
value94 <x_x9403@163.com>
2020-08-13 19:39:08 +0800

    add check apple show data

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -10,6 +10,7 @@
 // +----------------------------------------------------------------------
 namespace app\admin\controller;
 
+use app\admin\model\CheckAppleModel;
 use app\admin\model\EmailModel;
 use app\admin\model\MachineModel;
 use app\admin\model\NodeModel;
@@ -66,19 +67,56 @@ class Index extends Base
         /*$hour_success_reg = EmailModel::where(['reg_status' => 1])
             ->whereTime('update_time', '-1 hour')
             ->count('id');*/
+
+        // 总过检，，
+        $all_check = CheckAppleModel::where(['check_status' => [0, 1]])->count('id');
+        // 过检失败
+        $fail_check = CheckAppleModel::where(['check_status' => 0])->count('id');
+        // 过检成功
+        $success_check = CheckAppleModel::where(['check_status' => 1])->count('id');
+        // 剩余过检数
+        $left_check = CheckAppleModel::where(['check_status' => 2])->count('id');
+        // 今天过检总数，成功，失败总数
+        $today_check = CheckAppleModel::where(['check_status' => [0, 1]])
+            ->whereTime('update_time', 'today')
+            ->count('id');
+        $today_fail_check = CheckAppleModel::where(['check_status' => 0])
+            ->whereTime('update_time', 'today')
+            ->count('id');
+        // 上小时的过检总数，成功，失败数
+        $hour_check = CheckAppleModel::where(['check_status' => [0, 1]])
+            ->whereTime('update_time', '-1 hours')
+            ->count('id');
+        $hour_fail_check = CheckAppleModel::where(['check_status' => 0])
+            ->whereTime('update_time', '-1 hour')
+            ->count('id');
+
         $this->assign([
             'show_data' => [
+                // 激活数据统计
                 'all_reg' => $all_reg,
                 'fail_reg' => $fail_reg,
                 'success_reg' => $success_reg,
-                'machine_left' => $machine_left,
-                'email_left' => $email_left,
                 'today_reg' => $today_reg,
                 'today_fail_reg' => $today_fail_reg,
                 'today_success_reg' => $today_reg - $today_fail_reg,
                 'hour_reg' => $hour_reg,
                 'hour_fail_reg' => $hour_fail_reg,
-                'hour_success_reg' => $hour_reg - $hour_fail_reg
+                'hour_success_reg' => $hour_reg - $hour_fail_reg,
+                // 过检数据统计
+                'all_check' => $all_check,
+                'fail_check' => $fail_check,
+                'success_check' => $success_check,
+                'today_check' => $today_check,
+                'today_fail_check' => $today_fail_check,
+                'today_success_check' => $today_check - $today_fail_check,
+                'hour_check' => $hour_check,
+                'hour_fail_check' => $hour_fail_check,
+                'hour_success_check' => $hour_check - $hour_fail_check,
+
+                'machine_left' => $machine_left,
+                'email_left' => $email_left,
+
             ]
         ]);
         return $this->fetch('index');
@@ -94,8 +132,15 @@ class Index extends Base
         $all_reg = EmailModel::where(['reg_status' => [0, 1]])->where($where)->count('id');
         $fail_reg = EmailModel::where(['reg_status' => 0])->where($where)->count('id');
         $success_reg = EmailModel::where(['reg_status' => 1])->where($where)->count('id');
+        // 查询 总过检数量，成功数量，失败数量
+        $all_check = CheckAppleModel::where(['check_status' => [0, 1]])->where($where)->count('id');
+        $fail_check = CheckAppleModel::where(['check_status' => 0])->where($where)->count('id');
+        $success_check = CheckAppleModel::where(['check_status' => 1])->where($where)->count('id');
         return json([
             'code' => 1,
+            'all_check' =>$all_check,
+            'fail_check' =>$fail_check,
+            'success_check' =>$success_check,
             'all_reg' =>$all_reg,
             'fail_reg' =>$fail_reg,
             'success_reg' =>$success_reg,
